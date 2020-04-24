f859062c72cde4f7ef3ef48ef2ba48a986fa2255
Kiko Bela <kiko_bela_mkd@hotmail.com>
2020-04-24 19:13:38 +0200

    Add project files.

---
+++ Core/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int ddvId { get; set; }
+        public DDV DDV { get; set; }
+    }
+}
